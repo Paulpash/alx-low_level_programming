10883db9776c6375bc6d75f43fcbf859a9f8eefe
Paul_Lubega <lubegapaul39@gmail.com>
2022-09-09 02:53:04 -0700

    quick fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,9 +15,9 @@ int main(void)
 	long long lli;
 	float f;
 
-	printf("size of a char: %1d byte(s)\n", sizeof(c));
-	printf("size of an int: %1d byte(s)\n", sizeof(i));
-	printf("size of a long: %1d byte(s)\n", sizeof(li));
-	printf("size of a long long: %1d byte(s)\n", sizeof(lli));
-	printf("size of a float: %1d byte(s)\n", sizeof(f));
+	printf("Size of a char: %ld byte(s)\n", sizeof(c));
+	printf("Size of an int: %ld byte(s)\n", sizeof(i));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
+	printf("Size of a float: %ld byte(s)\n", sizeof(f));
 	retun (0);

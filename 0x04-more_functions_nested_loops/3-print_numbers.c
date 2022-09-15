a95b34d2591160ae6fdd30cc0f510f2242b39a0c
Paul_Lubega <lubegapaul39@gmail.com>
2022-09-15 03:21:58 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,6 +4,7 @@
  * print_numbers - prints the numbers, from 0 to 9,
  * followed by a new line
  */
+
 void print_numbers(void)
 {
 	int i;
@@ -12,5 +13,6 @@ void print_numbers(void)
 	{
 		_putchar(i + '0');
 	}
+
 	_putchar('\n');
 }

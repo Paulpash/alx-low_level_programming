2922c9b78fcfeae438a560400542cbc1788922dd
Paul_Lubega <lubegapaul39@gmail.com>
2022-09-16 10:50:07 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,20 +6,21 @@
  * 
  * Return: 0
  */
+
 void more_numbers(void)
 {
 int i, j;
 
-for (i = 0; i < 10; i++)
-{
-for (j = 0; j < 14; j++)
-{
-if (j > 9)
-{
-putchar(j / 10 + '0');
-}
-putchar(j % 10 + '0');
-}
-putchar(10);
-}
+	for (i = 0; i < 10; i++)
+	{
+	for (j = 0; j < 14; j++)
+	{
+	if (j > 9)
+	{
+	putchar((j / 10) + '0');
+	}
+	putchar((j % 10) + '0');
+	}
+	putchar(10);
+	}
 }

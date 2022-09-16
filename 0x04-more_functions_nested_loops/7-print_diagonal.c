03676b59eca6654a354fed0361eca4d96f8f8e5a
Paul_Lubega <lubegapaul39@gmail.com>
2022-09-16 14:48:06 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,18 +6,23 @@
  *@n: parameter
  *Return: 0
  */
+
 void print_diagonal(int n)
 {
 int i, j;
+
 if (n > 0)
 {
+
 for (i = 0; i < n; i++)
 {
+
 for (j = 0; j < n; j++)
 {
 putchar(' ');
 }
 putchar('\\');
+
 if (i == (n - 1))
 {
 continue;

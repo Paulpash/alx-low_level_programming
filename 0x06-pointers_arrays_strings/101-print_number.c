3c64b60a54c7a3fff4eec154af59936ce4bc9106
Paul_Lubega <lubegapaul39@gmail.com>
2022-09-21 03:34:09 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,18 +6,23 @@
  */
 void print_number(int n)
 {
-	unsigned int n1;
 
-	if (n < 0)
-	{
-		n1 = -n;
-		_putchar('-');
-	} else 
-	{
-		n1 = n;
-	}
-	if (n1 / 10)
-		print_number(n1 / 10);
+unsigned int n1;
 
-	_putchar((n1 % 10) + '0');
+if (n < 0)
+{
+n1 = -n;
+_putchar('-');
+
+}
+else
+{
+n1 = n;
+}
+
+if (n1 / 10)
+
+print_number(n1 / 10);
+
+_putchar((n1 % 10) + '0');
 }

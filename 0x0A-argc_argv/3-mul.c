18d294334514f2bc3419d7cc81f0daffbcf500ef
Paul_Lubega <lubegapaul39@gmail.com>
2022-10-03 06:09:18 -0700

    quick fix

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,22 +11,16 @@
  * Return: 0 if true, 1 if false
  *
  */
-
 int main(int argc, char *argv[])
 {
 int a, b;
 if (argc == 3)
-	
 {
-		a = atoi(argv[1]);
-		b = atoi(argv[2]);
-
-		printf("%d\n", a * b);
-
-		return (0);
+a = atoi(argv[1]);
+b = atoi(argv[2]);
+printf("%d\n", a *b);
+return (0);
 	}
-
-		printf("Error\n");
-
-		return (1);
-	}
+printf("Error\n");
+return (1);
+}
